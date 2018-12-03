5dbc0e8ea227506690954f491e5b9c5c0841c885
Neil Gall <neilgall@mac.com>
2018-12-03 08:44:29 +0000

    Day 3 work in progress

--- src/day3/day3.kt
+++ src/day3/day3.kt
@@ -1,15 +1,26 @@
 package adventofcode2018.day3
 
 import java.io.File
+import org.jparsec.Parser
+import org.jparsec.Parsers.*
+import org.jparsec.Scanners.*
 
-data class Claim(
-    val left: Int,
-    val top: Int,
-    val width: Int,
-    val height: Int
-)
+data class Origin(val left: Int, val top: Int)
+data class Size(val width: Int, val height: Int)
+data class Claim(val id: Int, val origin: Origin, val size: Size)
 
-fun main(arg: Array<String>) {
-    println("Part 1")
+val integer: Parser<Int> = INTEGER.map(String::toInt)
+fun <T> integerPair(sep: Char, c: (Int, Int) -> T): Parser<T> =  
+    sequence(integer.followedBy(isChar(sep)), integer, c)
+
+val claimId: Parser<Int> = isChar('#').next(integer)
+val origin: Parser<Origin> = WHITESPACES.followedBy(isChar('@')).followedBy(WHITESPACES).next(integerPair(',', ::Origin))
+val size: Parser<Size> = isChar(':').followedBy(WHITESPACES).next(integerPair('x', ::Size))
+val claim: Parser<Claim> = sequence(claimId, origin, size, ::Claim)
+
+fun main(args: Array<String>) {
+    val input: List<Claim> = File(args[0]).readLines().map(claim::parse)
+
+    println("Part 1, ${input}")
 }
 
