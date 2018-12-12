6ce47f639618b54a5d08424b44603a28d8801c34
Neil Gall <neilgall@mac.com>
2018-12-12 08:25:15 +0000

    Binary division of squares. Easy enough in one dimension

--- src/day11/day11.kt
+++ src/day11/day11.kt
@@ -1,18 +1,55 @@
 package adventofcode2018.day11
+
+import adventofcode2018.toolbox
 import java.io.File
 
+val IntRange.steps: Sequence<Int> get() = sequence {
+    var i: Int = start
+    var end: Int = endInclusive+1
+    var bit: Int = 1
+    do {
+        if (i and bit != 0) {
+            yield(bit)
+            i += bit
+        }
+        bit *= 2
+    } while (i + bit <= end)
+    do {
+        bit /= 2
+        if (i + bit <= end) {
+            yield(bit)
+            i += bit
+        }
+    } while (bit > 1)
+}
+
 sealed class Pos {
-    data class Single(val x: Int, val y: Int): Pos()
+    data class Atom(val x: Int, val y: Int): Pos()
+    
     data class Square(val x: Int, val y: Int, val size: Int): Pos() {
-        val cells: Sequence<Single> get() =
-            (x..x+size-1).asSequence().flatMap { sx ->
-                (y..y+size-1).asSequence().map { sy -> Single(sx, sy) }
+        val xRange: IntRange = x .. (x+size-1)
+        val yRange: IntRange = y .. (y+size-1)
+
+        val cells: Sequence<Pos> get() = when(size) {
+            1 -> sequenceOf(Atom(x, y))
+            2 -> sequenceOf(Atom(x, y), Atom(x+1, y), Atom(x, y+1), Atom(x+1, y+1))
+            else -> 
+                val qtr = size / 2
+                yield(Square(x,     y,     qtr))
+                yield(Square(x+qtr, y,     qtr))
+                yield(Square(x,     y+qtr, qtr))
+                yield(Square(x+qtr, y+qtr, qtr))
+                if (size and 1 != 0) {
+                    yieldAll((x..x+size-1).asSequence().map { i -> Pos.Atom(i, y+size-1) })
+                    yieldAll((y..y+size-1).asSequence().map { i -> Pos.Atom(x+size-1, i) })
+                }
             }
+        }
     }
 }
 
-fun Pos.Single.rackId(): Int = x + 10
-fun Pos.Single.startLevel(): Int = rackId() * y
+fun Pos.Atom.rackId(): Int = x + 10
+fun Pos.Atom.startLevel(): Int = rackId() * y
 fun Int.hundreds(): Int = (this / 100) % 10
 
 val nines: Sequence<Pos.Square> get() =
@@ -30,9 +67,11 @@ val squares: Sequence<Pos.Square> get() =
     }
 
 data class Grid(val serial: Int) {
+    val cache = mutableMapOf<Pos, Int>()
+
     operator fun get(pos: Pos): Int = when(pos) {
-        is Pos.Single -> ((pos.startLevel() + serial) * pos.rackId()).hundreds() - 5
-        is Pos.Square -> pos.cells.asSequence().map(this::get).sum()
+        is Pos.Atom -> ((pos.startLevel() + serial) * pos.rackId()).hundreds() - 5
+        is Pos.Square -> cache.getOrPut(pos) { pos.cells.asSequence().map(this::get).sum() }
     }
 }
 
@@ -42,25 +81,34 @@ fun part1(grid: Grid) {
     println("Part 1: ${max}")
     println(
         (max.y-1..max.y+3).map { y ->
-            (max.x-1..max.x+3).map { x -> grid[Pos.Single(x, y)].toString().padStart(4, ' ') }.joinToString(" ")
+            (max.x-1..max.x+3).map { x -> grid[Pos.Atom(x, y)].toString().padStart(4, ' ') }.joinToString(" ")
         }.joinToString("\n")
     )
 }
 
 fun part2(grid: Grid) {
+    // squares.forEach { sq -> println("${sq} ${sq.cells.toList()}") }
+
     val max: Pos.Square = squares.maxBy(grid::get) ?: throw IllegalStateException()
 
     println("Part 2: ${max}")
     println(
         (max.y-1..max.y+max.size+1).map { y ->
-            (max.x-1..max.x+max.size+1).map { x -> grid[Pos.Single(x, y)].toString().padStart(4, ' ') }.joinToString(" ")
+            (max.x-1..max.x+max.size+1).map { x -> grid[Pos.Atom(x, y)].toString().padStart(4, ' ') }.joinToString(" ")
         }.joinToString("\n")
     )
 }
 
 fun main(vararg args: String) {
-    val serial = File(args[0]).readText().trim().toInt()
-    val grid = Grid(serial)
-    part1(grid)
-    part2(grid)
+    // val serial = File(args[0]).readText().trim().toInt()
+    // val grid = Grid(serial)
+    // part1(grid)
+    // part2(grid)
+
+    (2..100).forEach { size -> 
+        (1..(101-size)).forEach { i ->
+            val r = IntRange(i, i+size-1)
+            println("$r ${r.steps.toList()}")
+        }
+    }
 }
